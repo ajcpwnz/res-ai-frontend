6acef86152cba6fd49f48dd45a66fb84000b0146
Alex Pasichnichenko <opasichnichenko@digitalsamba.com>
2025-04-16 22:20:48 +0300

    update UI

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-const API_HOST = 'http://localhost:5005'
-// const API_HOST = 'http://159.89.91.230:5000'
+// const API_HOST = 'http://localhost:5005'
+const API_HOST = 'http://159.89.91.230:5000'
 
 export const http = axios.create({
   baseURL: `${API_HOST}/v1`,
