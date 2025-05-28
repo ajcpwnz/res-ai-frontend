22b3ceca68e0f728433ff34e75702f3e44d37498
alex <ajcpwnz@d3feat.website>
2025-05-28 17:55:14 +0300

    update api url

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const API_HOST = '//dev-api.underwritewise.com:5005'
+export const API_HOST = 'https://dev-api.underwritewise.com'
 // export const API_HOST = 'http://67.205.183.132:5005'
 
 export const http = axios.create({
